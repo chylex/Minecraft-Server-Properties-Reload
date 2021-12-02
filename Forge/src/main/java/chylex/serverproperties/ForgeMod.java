0a80d9b5d0ff8aa23170242b127a31e258731f2a
chylex <contact@chylex.com>
2021-12-02 08:14:22 +0100

    Update Forge version to Minecraft 1.18+

--- Forge/src/main/java/chylex/serverproperties/ForgeMod.java
+++ Forge/src/main/java/chylex/serverproperties/ForgeMod.java
@@ -2,11 +2,11 @@ package chylex.serverproperties;
 import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod("serverpropertiesreload")
 public final class ForgeMod {
 	public ForgeMod() {
-		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 }
